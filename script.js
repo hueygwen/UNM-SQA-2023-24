fa3e7723b1bb49a801f2d99d68063d2519429d00
hfyzy4 <hfyzy4@nottingham.edu.my>
2023-10-30 10:30:46 +0800

    Version 1.01
    Add iframe.html
    Made changes on style.css

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=Software%20Quality%20Assurance&type=video&videoDuration=short&key=AIzaSyBQq7MpU8_NqDC34ujWCWMtOWnqrGhOErA")
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=Software%20Quality%20Assurance&type=video&videoDuration=short&key=AIzaSyCeWB_UtM_jMkxmWgk5OJjxhXEVUzfrGYU")
 .then((response) => {
     return response.json();
 })
 .then((data) => {
     console.log(data)
     let videos = data.items
-    let videoContainer = document.querySelector(".test-container .row")
+    let videoContainer = document.querySelector(".video-container .row")
+    var video
     
     for(video of videos) {
         
         videoContainer.innerHTML += `
             
             <div class="col-md-4 test-col">
-            <a href="https://www.youtube.com/watch?v=${video.id.videoId}" class="youtube-link" target="_blank">
-            <img src="${video.snippet.thumbnails.high.url}" alt="Youtube Thumbnail">
-            <h5 class="youtube-title">${video.snippet.title}</h5>    
-            <p class="youtube-channel">${video.snippet.channelTitle}</p>
-            </a>
-            </div>`
+                <a href="https://www.youtube.com/watch?v=${video.id.videoId}" class="youtube-link" target="_blank">
+                <img src="${video.snippet.thumbnails.high.url}" alt="Youtube Thumbnail">
+                <h5 class="youtube-title">${video.snippet.title}</h5>    
+                <p class="youtube-channel">${video.snippet.channelTitle}</p>
+                </a>
+            </div>
+        `
     }
 })
