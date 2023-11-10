1b423c1ce0bc46625211e2e11e179305de62e39d
hfyzy4 <hfyzy4@nottingham.edu.my>
2023-11-11 03:15:08 +0800

    Update iFrame.html and CSS
    1. Solved issue for repeated notification board for copy link
    2. Combine notes with suggested videos is completed
    3. Made some CSS changes for responsiveness

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=Software%20Quality%20Assurance&type=video&videoDuration=short&key=AIzaSyCeWB_UtM_jMkxmWgk5OJjxhXEVUzfrGYU")
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=Software%20Quality%20Assurance&type=video&videoDuration=short&key=AIzaSyBQq7MpU8_NqDC34ujWCWMtOWnqrGhOErA")
 .then((response) => {
     return response.json();
 })
@@ -8,23 +8,61 @@ fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=
     let videoContainer = document.querySelector(".video-container .row")
     var video
     
-    for(video of videos) {
+    var pathName = window.location.pathname
+    var pageName = pathName.split("/")
+
+    let suggestContainer = document.querySelector(".suggest-col")
+    
+    if (pageName.includes("Home.html")) {
+        for(video of videos) {
         
-        videoContainer.innerHTML += `
-            
-            <div class="col-md-4 test-col">
-                <a href="iFrame.html?videoID=${video.id.videoId}" class="youtube-link">
-                <img src="${video.snippet.thumbnails.high.url}" alt="Youtube Thumbnail">
-                <h5 class="youtube-title">${video.snippet.title}</h5>    
-                <p class="youtube-channel">${video.snippet.channelTitle}</p>
+            videoContainer.innerHTML += `
+                
+                <div class="col-md-4 test-col">
+                    <a href="iFrame.html?videoID=${video.id.videoId}" class="youtube-link">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="Youtube Thumbnail" class="youtube-thumbnail">
+                    <h5 class="youtube-title">${video.snippet.title}</h5>    
+                    <p class="youtube-channel">${video.snippet.channelTitle}</p>
+                    </a>
+                </div>
+            `
+        }
+
+    } else if (pageName.includes("iFrame.html")) {
+        let checkVideoUrl = document.querySelector("iframe")
+        let videoUrl = checkVideoUrl.src
+        let checkVideoID = videoUrl.split("/")
+        let videoID = checkVideoID[checkVideoID.length - 1]
+
+        console.log(videoID)
+
+        for(video of videos) {
+
+            if (videoID == `${video.id.videoId}`) {
+                /* video.length + 1 */
+                continue
+            }
+
+            suggestContainer.innerHTML += `
+
+                <a href="iFrame.html?videoID=${video.id.videoId}">
+                    <div class="row g-3 suggest-video-row">
+                        <div class="col-4">
+                            <img src="${video.snippet.thumbnails.high.url}" alt="Youtube Thumbnail" class="youtube-thumbnail">
+                        </div>
+
+                        <div class="col-8">
+                            <h5 class="suggest-title">${video.snippet.title}</h5>
+                            <p class="suggest-channel">${video.snippet.channelTitle}</p>
+                        </div>
+                    </div>
                 </a>
-            </div>
-        `
-        
-
+                 
+            `
+        }
     }
+    
 })
-
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -35,6 +73,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let playVideo = document.querySelector('iframe');
     playVideo.src = `https://www.youtube.com/embed/${videoID}`;
+
+    document.getElementById("copy-link-btn").onclick = function() {copyPageLinkToClipboard()};
+
 
     // Function to copy the page link to the clipboard
     function copyPageLinkToClipboard() {
@@ -57,8 +98,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Add an event listener to the "Copy" button
-    const copyLinkBtn = document.getElementById("copy-link-btn");
-    copyLinkBtn.addEventListener("click", copyPageLinkToClipboard);
+    /* const copyLinkBtn = document.getElementById("copy-link-btn");
+    copyLinkBtn.addEventListener("click", copyPageLinkToClipboard); */
+
+
 });
 
 
