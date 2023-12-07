57b62901a628b2821b3c1a1ec8c2902aa7faf7e4
hfyzy4 <116628750+hfyzy4@users.noreply.github.com>
2023-12-07 14:53:56 +0800

    Change API Key for Selenium Test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=Software%20Quality%20Assurance&type=video&videoDuration=short&key=AIzaSyD739F5j3-ucKW_-Hj6Q1_cUXiKPn6dUc0")
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=Software%20Quality%20Assurance&type=video&videoDuration=short&key=AIzaSyCyPqh9wlT0EfvhnB97RcrBjmYIMc_o9HM")
 .then((response) => {
     return response.json();
 })
